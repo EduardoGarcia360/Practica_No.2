5bb2afc409956150f0f60013eacba963ae9d4201
EduardoGarcia360 <eduardo_garcia360@hotmail.com>
2014-10-28 22:50:13 -0600

    cambios 1
    agregando metodos que hacen falta

--- Practica2/src/Main.java
+++ Practica2/src/Main.java
@@ -8,22 +8,39 @@ public class Main {
 		String ele="", busca="";
 		
 		do{
-			System.out.println("----------MENU----------\n1. Agregar.\n2. Mostrar Elementos."
-					+ "\n3. Buscar.\n4. Eliminar.\n5. Modificar.\n6. Salir");
+			System.out.println("----------MENU----------\n"
+					+ "1. Agregar al Inicio.\n"
+					+ "2. Agregar al Final.\n"
+					+ "3. Mostrar Elementos\n"
+					+ "4. Tamaño de la Lista.\n"
+					+ "5. Buscar.\n"
+					+ "6. Agregar en Posicion.\n"
+					+ "7. Obtener Posicion\n"
+					+ "8. Eliminar Posicion\n"
+					+ "9. Salir");
 			Scanner o = new Scanner(System.in);
 			opcion=o.nextInt();
 			
 			switch(opcion){
 			case 1:
 				System.out.print("Ingrese el elemento: ");
-				Scanner e = new Scanner(System.in);
-				ele=e.next();
-				list.Agregar(ele);
+				Scanner ai = new Scanner(System.in);
+				ele=ai.next();
+				list.AgregarInicio(ele);
 				break;
 			case 2:
-				list.Imprimir();
+				System.out.print("Ingrese el elemento: ");
+				Scanner af = new Scanner(System.in);
+				ele=af.next();
+				list.AgregarFinal(ele);
 				break;
 			case 3:
+				list.Imprimir();
+				break;
+			case 4:
+				list.Tamaño();
+				break;
+			case 5:
 				System.out.print("Ingrese la busqueda: ");
 				Scanner abu = new Scanner(System.in);
 				busca=abu.next();
@@ -33,15 +50,47 @@ public class Main {
 					System.out.println("El elemento "+busca+" no fue encontrado.");
 				}
 				break;
-			case 4:
-				
-				list.Eliminar();
+			case 6:
+				if(list.Vacia()){
+					System.out.println("Lista Vacia");
+				}else{
+					System.out.println("----Ingresar en posicion----\n"
+							+ "Tome en cuenta que las posiciones estan dadas de la siguiente manera.\n"
+							+ "[1]->[2]->[3]->...->[n]->||\n"
+							+ "Ingrese una posicion a modificar: ");
+					Scanner po = new Scanner(System.in);
+					int posicion = po.nextInt();
+					list.AgregarPosicion(posicion);
+				}
 				break;
-			case 5:
-				list.Modificar();
+			case 7:
+				if(list.Vacia()){
+					System.out.println("Lista Vacia");
+				}else{
+					System.out.println("----Obtener posicion----\n"
+							+ "Tome en cuenta que las posiciones estan dadas de la siguiente manera.\n"
+							+ "[1]->[2]->[3]->...->[n]->||\n"
+							+ "Ingrese una posicion a mostrar: ");
+					Scanner po = new Scanner(System.in);
+					int posicion = po.nextInt();
+					list.Obtener(posicion);
+				}
+				break;
+			case 8:
+				if(list.Vacia()){
+					System.out.println("Lista Vacia");
+				}else{
+					System.out.println("----Eliminar posicion----\n"
+							+ "Tome en cuenta que las posiciones estan dadas de la siguiente manera.\n"
+							+ "[1]->[2]->[3]->...->[n]->||\n"
+							+ "Ingrese una posicion para eliminar: ");
+					Scanner po = new Scanner(System.in);
+					int posicion = po.nextInt();
+					list.Eliminar(posicion);
+				}
 				break;
 			}
-		}while(opcion!=6);
+		}while(opcion!=9);
 
 	}
 
